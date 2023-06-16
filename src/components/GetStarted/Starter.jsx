e4f78cb78ce906fc0de23b4b8226a69fa0901594
asadbek08 <s290978@studenti.polito.it>
2023-06-16 03:23:44 +0200

    Created All

---
+++ src/components/GetStarted/Starter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Starter.css'
+const Starter = () => {
+  return (
+    <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexCenter inner-container">
+                <span class='primaryText'>Get Started with Homyz</span>
+                <span class='secondaryText'>Subscribe and find super prices from variety of choices</span>          
+                <button className="button">
+                  <a href="mailto:asad19990826@gmail.com" className="">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Starter
